aecd210e66041928a8a33d6692c43172499f6040
Leonardo Passeri <passeri.leonardo@gmail.com>
2017-12-12 16:00:33 +0100

    Refactor: split swap transition in separate states to get a finer grained control over them.

--- ParallelWorlds/Assets/Scripts/PlayerUniverse.cs
+++ ParallelWorlds/Assets/Scripts/PlayerUniverse.cs
@@ -7,11 +7,28 @@ using UnityEngine.Networking;
 public class IntEvent : UnityEvent<int> { }
 public class PlayerUniverse : NetworkBehaviour
 {
+    public struct PlayerUniverseState
+    {
+        public enum TransitionState
+        {
+            Normal,
+            SwapIn,
+            SwapOut
+        }
+
+        public PlayerUniverseState(int universe, TransitionState transitionState)
+        {
+            this.universe = universe;
+            this.transitionState = transitionState;
+        }
+
+        public int universe { get; private set; }
+        public TransitionState transitionState { get; private set; }
+    }
+
     [SerializeField] IntEvent onSwitchUniverseShared;
 
-    [SyncVar(hook = "OnPlayerUniverseChange")] int playerUniverse;
-
-    [HideInInspector] public bool Swapping { get; private set; }
+    [SyncVar(hook = "OnPlayerUniverseStateChange")] PlayerUniverseState playerUniverseState;
 
     [Header("Swap Effect Stuff")]
     // [SerializeField] private Vingette _vingette;
@@ -21,11 +38,9 @@ public class PlayerUniverse : NetworkBehaviour
     [SerializeField] private AnimationCurve _outerVingette;
     [SerializeField] private AnimationCurve _saturation;
     [SerializeField] private AnimationCurve _fov;
-    [SerializeField] private AnimationCurve _timeScale;
     [SerializeField] private AudioClip _swapAudioClip;
 
     private AudioSource _audio;
-    private bool _swapTiggered;
     private readonly float _swapTime = 0.85f;
 
     private void Awake()
@@ -47,105 +62,154 @@ public class PlayerUniverse : NetworkBehaviour
     [ServerCallback]
     private void OnEnable()
     {
-        playerUniverse = UniverseController.Instance.GetSpawnUniverse();
+        playerUniverseState = new PlayerUniverseState(
+            UniverseController.Instance.GetSpawnUniverse(),
+            PlayerUniverseState.TransitionState.Normal
+        );
     }
 
     private void Update()
     {
         if (isLocalPlayer)
         {
-            if (!Swapping && Input.GetMouseButtonDown(1))
+            if (playerUniverseState.transitionState == PlayerUniverseState.TransitionState.Normal && Input.GetMouseButtonDown(1))
             {
-                Swapping = true;
-                CmdSwitchToOppositeUniverse();
+                CmdStartSwapToOppositeUniverse();
             }
         }
     }
 
+    // --------------- COMMANDS ---------------
+
     [Command]
-    void CmdSwitchToOppositeUniverse()
+    void CmdStartSwapToOppositeUniverse()
     {
-        playerUniverse = UniverseController.Instance.GetOppositeUniverse(playerUniverse);
+        playerUniverseState = new PlayerUniverseState(
+            playerUniverseState.universe,
+            PlayerUniverseState.TransitionState.SwapOut
+        );
     }
 
-    void OnPlayerUniverseChange(int universe)
+    [Command]
+    void CmdSwapToOppositeUniverse()
     {
-        playerUniverse = universe;
+        playerUniverseState = new PlayerUniverseState(
+            UniverseController.Instance.GetOppositeUniverse(playerUniverseState.universe),
+            PlayerUniverseState.TransitionState.SwapIn
+        );
+    }
+
+    [Command]
+    void CmdStopSwapToOppositeUniverse()
+    {
+        playerUniverseState = new PlayerUniverseState(
+            playerUniverseState.universe,
+            PlayerUniverseState.TransitionState.Normal
+        );
+    }
+
+    // --------------- HOOKS ---------------
+
+    void OnPlayerUniverseStateChange(PlayerUniverseState universeState)
+    {
+        playerUniverseState = universeState;
 
         UpdatePlayerUniverse();
     }
 
+    // --------------- SWAP LOGIC ---------------
+
     void UpdatePlayerUniverse()
     {
-        if (playerUniverse != 0)
-        {
-            Debug.Log("Set Universe: " + playerUniverse + " for player net ID: " + netId);
-            
+        ResetSwapEffect();
 
-            if (Swapping)
+        if (playerUniverseState.universe != 0)
+        {
+            if (playerUniverseState.transitionState == PlayerUniverseState.TransitionState.Normal)
+            {
+                SwapInstant();
+            }
+            else
             {
                 if (isLocalPlayer)
                 {
-                    StartCoroutine(SwapAsync());
+                    if (playerUniverseState.transitionState == PlayerUniverseState.TransitionState.SwapOut)
+                    {
+                        Debug.Log("Player net ID: " + netId + "swapping out of universe: " + playerUniverseState.universe);
+                        StartCoroutine("SwapOutAsync");
+                    }
+                    else
+                    {
+                        Debug.Log("Player net ID: " + netId + " swapping in universe: " + playerUniverseState.universe);
+                        onSwitchUniverseShared.Invoke(playerUniverseState.universe);
+                        StartCoroutine("SwapInAsync");
+                    }
                 }
                 else
                 {
                     // TODO: Add transition effect as seen from other players
                 }
             }
-            else
-            {
-                SwapInstant();
-            }
         }
     }
 
+    // --------------- SWAP EFFECTS ---------------
+
     private void SwapInstant()
     {
-        onSwitchUniverseShared.Invoke(playerUniverse);
+        Debug.Log("Player net ID: " + netId + " arrived in universe: " + playerUniverseState.universe);
+        onSwitchUniverseShared.Invoke(playerUniverseState.universe);
+    }
+
+    private void ResetSwapEffect()
+    {
+        StopCoroutine("SwapOutAsync");
+        StopCoroutine("SwapInAsync");
+
+        if (playerUniverseState.transitionState == PlayerUniverseState.TransitionState.SwapOut)
+        {
+            ApplyEffect(_swapTime);
+        }
+        else
+        {
+            ApplyEffect(1.0f);
+        }
     }
 
     /// <summary>
 	/// Controls a bunch of stuff like vingette and FoV over time and change the cullmask of the player's camera after a fixed duration.
 	/// </summary>
-	private IEnumerator SwapAsync()
+	private IEnumerator SwapOutAsync()
     {
-        _swapTiggered = false;
-
         _audio.PlayOneShot(_swapAudioClip);
 
-        for (float t = 0; t < 1.0f; t += Time.unscaledDeltaTime * 1.2f)
+        for (float t = 0; t < _swapTime; t += Time.unscaledDeltaTime * 1.2f)
         {
-            _camera.fieldOfView = _fov.Evaluate(t);
-            /* _vingette.MinRadius = _innerVingette.Evaluate(t);
-			_vingette.MaxRadius = _outerVingette.Evaluate(t);
-			_vingette.Saturation = _saturation.Evaluate(t); */
-            Time.timeScale = _timeScale.Evaluate(t);
-
-            if (t > _swapTime && !_swapTiggered)
-            {
-                _swapTiggered = true;
-               onSwitchUniverseShared.Invoke(playerUniverse);
-            }
+            ApplyEffect(t);
 
             yield return null;
         }
 
-        // technically a huge lag spike could cause this to be missed in the coroutine so double check it here.
-        if (!_swapTiggered)
+        CmdSwapToOppositeUniverse();
+    }
+
+    private IEnumerator SwapInAsync()
+    {
+        for (float t = _swapTime; t < 1.0f; t += Time.unscaledDeltaTime * 1.2f)
         {
-            _swapTiggered = true;
-            onSwitchUniverseShared.Invoke(playerUniverse);
+            ApplyEffect(t);
+
+            yield return null;
         }
 
-        _camera.fieldOfView = _fov.Evaluate(1.0f);
+        CmdStopSwapToOppositeUniverse();
+    }
 
-        /* _vingette.MinRadius = _innerVingette.Evaluate(1.0f);
-		_vingette.MaxRadius = _outerVingette.Evaluate(1.0f);
-		_vingette.Saturation = 1.0f; */
-
-        Time.timeScale = 1.0f;
-
-        Swapping = false;
+    private void ApplyEffect(float t)
+    {
+        _camera.fieldOfView = _fov.Evaluate(t);
+        /* _vingette.MinRadius = _innerVingette.Evaluate(t);
+        _vingette.MaxRadius = _outerVingette.Evaluate(t);
+        _vingette.Saturation = _saturation.Evaluate(t); */
     }
 }
